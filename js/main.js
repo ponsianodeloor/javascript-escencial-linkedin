260c5d8f529828c56888fbfcb2a0b743afebbd4e
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-08-18 12:57:06 -0500

    3.4 Tipos de datos especificos Javascript
    Trabajar con fechas en javascript
    https://www.linkedin.com/learning/javascript-esencial/trabajar-con-fechas-en-javascript

--- js/main.js
+++ js/main.js
@@ -1,21 +1,10 @@
 "use strict";
 
-alert('Hola desde un archivo externo');
-console.log('Mensaje a la consola');
+var fecha = new Date();
 
-var nombre = "Ponsiano";
-console.log(nombre);
-
-
-function saludar(){
- let apellidos = 'De Loor';
- console.log(apellidos);
-}
-
-var edad = 30;
-
-var edadtxt = parseInt(edad);
-
-saludar();
-
-const pi = 3.1416;
+console.log(fecha);
+console.log(fecha.getDay());
+console.log(fecha.getHours());
+console.log(fecha.getMinutes());
+fecha.setDate(10)
+console.log(fecha);
