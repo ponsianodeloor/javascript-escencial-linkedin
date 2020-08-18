8cb440a6f6be4a9af8cccb3ed84f3a14d4464050
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-08-18 17:14:26 -0500

    4.7.- Uso de operadores en Javascript
    Operadores aritméticos
    suma +
    resta -
    multiplicación *
    división /
        Operaciones relacionales
            Mayor que >
            Menor que <
            Mayor o igual que >=
            Menor o igual que <=
            Igual que ==
            Distinto a !=
        Operadores lógicos
            AND
            OR
            NOT
        Operadores de asignación
            igual =
            sumar y asignar +=
            restar y asignar -=
        Operador negativo
    a  = 10;
    b = -a;
    Operador de concatenación
    a = “2”; b = “3”
    c = a + b;
    >> 23
        Operador ternario o condicional
            a = 10;
            b = 20;
            resultado = a > b ? “Si es mayor” : “No es mayor”

--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
 "use strict";
 
-//JSON Javascript Object Notation Douglas Crockford Javascript The Good Parts
+var a = 10;
+var b = 20;
 
-var persona = {nombre: "Ponsiano", twitter: 'ponsianodeloor'};
+var resultado = a > b ? "la variable a es mayor" : "la variable a es menor";
 
-var personas = [
- {nombre: "Ponsiano", twitter: 'ponsianodeloor'},
- {nombre: "Thomas", twitter: 'thomasloor'},
- {nombre: "PR7", twitter: 'pr7'},
- persona
-];
-
-//esto sirve para enviar datos a un servidor o guardar localmente
-var personaJson = JSON.stringify(persona);
-
-//esto sirve para recibir datos y parsearlos
-var personaJsonRecibir = JSON.parse(personaJson);
+console.log(resultado);
