83084cb0e9dadafdd324cf1ee3fa133649ffbba3
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-08-18 12:14:57 -0500

    2.- Uso de variables o contenedores de datos
    creacion de variables y uso de console
    https://www.linkedin.com/learning/javascript-esencial/contenedores-const

--- js/main.js
+++ js/main.js
@@ -2,3 +2,18 @@
 
 alert('Hola desde un archivo externo');
 console.log('Mensaje a la consola');
+
+var nombre = "Ponsiano";
+console.log(nombre);
+
+
+function saludar(){
+ let apellidos = 'De Loor';
+ console.log(apellidos);
+}
+
+var edad = 30;
+
+var edadtxt = parseInt(edad);
+
+saludar();
