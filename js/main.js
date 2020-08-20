11cae98f351106728fd76ea3c478f76ce223ef9f
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-08-19 23:03:01 -0500

    7.- Funciones o métodos
    Estructura básica
    Parámetros de una función
    Parámetros tipo rest
    Parámetros tipo spread

--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
 "use strict";
 
-var productos = 5;
-var contar_impares = 0;
-
-for (var i = 0; i < productos; i++) {
- console.log("Producto #", i);
+function saludar(nombre, edad){
+ //var saludo = "hola mundo";
+ //console.log(saludo);
+ var resultado = nombre + " tiene " + edad + " años";
+ return resultado;
 }
 
-do{
- console.log('Producto vendido');
- productos--;
- debugger;
-}while(productos >= 1)
+var mensaje = saludar("Ponsiano", 30);
 
-for (var i = 0; i <= 20; i++) {
- if (i == 10) {
-  break;
- }
- if (i % 2 == 0) {
-   continue;
- }
- contar_impares++;
- console.log(i);
+function contar(cantidad = 20){
+ console.log('Total', cantidad);
 }
+
+//parametros rest
+function cocinar(ingrediente_1, ingrediente_2, ...masingredientes){
+ console.log('ingrediente 1', ingrediente_1);
+ console.log('ingrediente 2', ingrediente_2);
+ console.log('ingrediente 3', masingredientes);
+}
+
+cocinar("Pollo", "Tomate", "Pescado", "Chile", "Pimiento")
+
+function cocinar_x_parametros(...masingredientes){
+ console.log('ingredientes', masingredientes);
+}
+
+cocinar_x_parametros("Pollo", "Tomate", "Pescado", "Chile", "Pimiento");
+
+//parametros spread
+function cocinarSpread(ingrediente_1, ingrediente_2, ingrediente_3, ...otros){
+ console.log('ingrediente 1', ingrediente_1);
+ console.log('ingrediente 2', ingrediente_2);
+ console.log('ingrediente 3', ingrediente_3);
+ console.log('ingrediente otros', otros);
+}
+var ingredientesBase = ["Arroz, Pollo, Pimiento, Cebolla"];
+cocinarSpread(...ingredientesBase, "Arroz", "Tomate", "Cebolla blanca");
