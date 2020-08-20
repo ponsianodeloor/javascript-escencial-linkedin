ade1c84488c80f7b2cf8e66734dde0e24b13e80e
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-08-19 23:09:00 -0500

    7.5.- Funciones o métodos
    Funciones anonimas

--- js/main.js
+++ js/main.js
@@ -1,39 +1,16 @@
 "use strict";
 
-function saludar(nombre, edad){
- //var saludo = "hola mundo";
- //console.log(saludo);
- var resultado = nombre + " tiene " + edad + " años";
- return resultado;
+//funciones anonima
+/*
+(
+ function(){
+  var mensaje = "Hola con funcion anonima";
+  console.log(mensaje);
+ }
+)()
+*/
+
+var saludar = function(nombre){
+ var mensaje = "Hola " + nombre;
+ return mensaje;
 }
-
-var mensaje = saludar("Ponsiano", 30);
-
-function contar(cantidad = 20){
- console.log('Total', cantidad);
-}
-
-//parametros rest
-function cocinar(ingrediente_1, ingrediente_2, ...masingredientes){
- console.log('ingrediente 1', ingrediente_1);
- console.log('ingrediente 2', ingrediente_2);
- console.log('ingrediente 3', masingredientes);
-}
-
-cocinar("Pollo", "Tomate", "Pescado", "Chile", "Pimiento")
-
-function cocinar_x_parametros(...masingredientes){
- console.log('ingredientes', masingredientes);
-}
-
-cocinar_x_parametros("Pollo", "Tomate", "Pescado", "Chile", "Pimiento");
-
-//parametros spread
-function cocinarSpread(ingrediente_1, ingrediente_2, ingrediente_3, ...otros){
- console.log('ingrediente 1', ingrediente_1);
- console.log('ingrediente 2', ingrediente_2);
- console.log('ingrediente 3', ingrediente_3);
- console.log('ingrediente otros', otros);
-}
-var ingredientesBase = ["Arroz, Pollo, Pimiento, Cebolla"];
-cocinarSpread(...ingredientesBase, "Arroz", "Tomate", "Cebolla blanca");
