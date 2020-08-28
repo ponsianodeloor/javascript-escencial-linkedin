a497e73518aaa25047e3a1d36829f74058e932ea
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-08-28 06:48:45 -0500

    8.- Trabajar con eventos
    8.1 Eventos del mouse

--- js/main.js
+++ js/main.js
@@ -2,12 +2,18 @@
 
 const boton = document.querySelector('.boton');
 
+
 boton.addEventListener('click', function(){
- //console.log('Boton pulsado');
- console.log(this.innerHTML);
+ console.log('Boton pulsado');
+ //console.log(this.innerHTML);
 });
 
-boton.addEventListener('click', () => {
- console.log(this);
- this.location = 'https://apptics.com.ec'
+boton.addEventListener('mouseover', function(){
+ console.log('El mouse esta sobre el boton');
+ //console.log(this.innerHTML);
+});
+
+boton.addEventListener('mouseout', function(){
+ console.log('El mouse esta fuera el boton');
+ //console.log(this.innerHTML);
 });
