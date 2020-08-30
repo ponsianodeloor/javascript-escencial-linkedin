ab435a7107de36afe21f00ccb6553d6eb093e3a6
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-08-30 13:03:07 -0500

    8.2.- Eventos multimedia
    Carga del video

--- js/main.js
+++ js/main.js
@@ -1,28 +1,16 @@
 "use strict";
 
-/*
- eventos del teclado
-  keydown
-  keypress
-  keyup
-*/
+//eventos multimedia
+const video = document.querySelector('.laravelVideo');
 
-
-window.addEventListener("keydown", function(event){
- console.log("keydown tecla");
- console.log(event.keyCode);
- console.log(String.fromCharCode(event.keyCode));
+video.addEventListener("play", function(){
+ console.log('El video ha iniciado');
 });
 
-window.addEventListener("keypress", function(event){
- console.log("keypress tecla");
+video.addEventListener("seeking", function(){
+ console.log('Se esta buscando en el video', this.currentTime);
 });
 
-window.addEventListener("keyup", function(event){
- console.log("keyup tecla");
-});
-
-//evento de carga del documento
-window.addEventListener("load", function(event){
- console.log("El contenido de la ventana se ha cargado");
+video.addEventListener("ended", function(){
+ console.log('El video ha terminado');
 });
