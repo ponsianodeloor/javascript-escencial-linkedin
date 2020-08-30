9b74c419d8c4fe02e5021d57447ff27ddd04fbf4
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-08-30 15:14:43 -0500

    9.- Alerts confirm y Prompt

--- js/main.js
+++ js/main.js
@@ -1,25 +1,18 @@
 "use strict";
 
-//temporizadores o timers
+//ventana de alerta
 
-//set interval se cicla
+var video = document.querySelector(".laravelVideo");
 
-var temporizador = setInterval(function () {
- setColor();
-}, 2000);
-
-//set timeout solo ocurre una vez
-/*
-setTimeout(function () {
- setColor();
-}, 3000);
-*/
-
-function setColor(){
- var pagina = document.body;
- pagina.style.backgroundColor = pagina.style.backgroundColor == "blue" ? "green" : "blue";
-}
-
-function stopChangeColor(){
- clearInterval(temporizador);
-}
+video.addEventListener('ended', function functionName() {
+ //alert('Video Finalizado \n salto de linea'); //alerta
+ let resultado = confirm("Deseas ver el video nuevamente");
+ console.log(resultado); //true o false
+ if (resultado) {
+  var email = prompt('Escribe tu email para ver mas videos');
+  console.log(email);
+  video.play();
+ }else {
+  window.location = "https://apptics.com.ec"
+ }
+});
