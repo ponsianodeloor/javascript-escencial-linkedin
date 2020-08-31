1227c29e16b0e95663b665bb51857247a8a09bec
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-08-31 14:17:41 -0500

    11.- Trabajando con cadenas de texto

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 "use strict";
 
-//metodos numericos
-var numero = 2.5;
+//cadena de texto
 
-console.log("toExponential", numero.toExponential(4));
-console.log("toFixed", numero.toFixed(2)); //para manejar 2 decimales
-console.log("toPrecision", numero.toPrecision(6));
-console.log("toString", numero.toString());
-console.log(typeof(numero.toString()))
+var pais = "comida";
+var comida = new String("Ceviche");
