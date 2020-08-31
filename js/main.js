b162b2afd625ee6879133e4e4673c9b127dab29d
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-08-31 15:28:51 -0500

    11.2- Trabajando con cadenas de texto
    Búsqueda de caracteres en cadenas de texto

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 "use strict";
 
-//medir una cadena de texto
-var mensaje = 'Ponsiano De Loor';
-console.log("La cadena de texto tiene " + mensaje.length + " letras");
+var mensaje = "Estoy aprendiendo Javascript y aprendiendo bien";
+
+var resultado;
+
+//indexof
+resultado = mensaje.indexOf('aprendiendo'); //nos encuentra la primera incidencia ASC
+console.log('indexOf ' + resultado);
+
+//lastIndexof
+resultado = mensaje.lastIndexOf('aprendiendo'); //nos encuentra la primera incidencia DESC
+console.log('lastIndexOf ' + resultado);
+
+//search
+resultado = mensaje.search('aprendiendo'); //nos encuentra la primera incidencia
+console.log('search ' + resultado);
+
+//search
+resultado = mensaje.search(/ja/i); // la i es para in case sensitive
+console.log('search case sensitive ' + resultado);
