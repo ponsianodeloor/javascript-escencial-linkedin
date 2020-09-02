74f5ce8ec0f21098f36227b15fc6f5856ad7f751
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-02 17:51:39 -0500

    12.5.- Trabajar con Arreglos
    Ordenando un arreglo

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
 "use strict";
 
-//operaciones basicas en un arreglo
+//ordenando un arreglo
+var platillos = ["Ceviche", "Tacos", "pasta"];
+console.log('Antes: ', platillos);
 
-//split()
-var mensaje = "Ceviche, Tacos, pasta";
-var platillos = mensaje.split(', ');
-console.log(platillos);
+platillos.sort();
+console.log('Después: ', platillos);
 
-//from
-var platillosHTML = Array.from(document.querySelectorAll(".platillos p"));
-platillos = platillosHTML.map(platillo => platillo.textContent)
-console.log(platillos);
-
-//of
-platillos = Array.of("Ceviche", "Tacos", "Pasta");
-console.log(platillos);
+platillos.reverse();
+console.log('Después Reverse: ', platillos);
