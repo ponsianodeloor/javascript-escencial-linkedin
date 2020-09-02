7dc93cb9ca8c460fefacba2c7715bbb9e09f8d05
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-02 15:03:20 -0500

    12.- Trabajar con Arreglos
    Crear primer arreglo

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 "use strict";
 
-var lenguaje = "Javascript";
-var lenguaje2 = "HTML";
+var platillos = ["ceviche", "bolon", "fideos"];
+var bebidas = new Array("Limonada", "Rosa", "Piña");
 
-var mensaje = `Me gusta ${lenguaje} y su integracion con el lenguaje ${lenguaje2}`;
-
-console.log(mensaje);
-
-var mensajeMultilinea = `
- Hola mundo
- Estoy aprendiendo ${lenguaje}
- y me gusta como se integra con ${lenguaje2} y CSS`;
-
-console.log(mensajeMultilinea);
+console.log(platillos, bebidas);
