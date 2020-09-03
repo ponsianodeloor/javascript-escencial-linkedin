9af5a1906bb500f28cb6ee8662e9dac91c430b3c
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-03 09:23:34 -0500

    12.6.- Trabajar con Arreglos
    Desestructuración de arreglos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 "use strict";
 
-//ordenando un arreglo
+//Desestructuracion de arreglos
 var platillos = ["Ceviche", "Tacos", "pasta"];
-console.log('Antes: ', platillos);
+/*
+var platillo1 = platillos[0];
+var platillo2 = platillos[1];
+var platillo3 = platillos[2];
+*/
 
-platillos.sort();
-console.log('Después: ', platillos);
+/*
+var platillo1 = null;
+var platillo2 = null;
+var platillo3 = null;
+*/
 
-platillos.reverse();
-console.log('Después Reverse: ', platillos);
+var [platillo1, platillo2, platillo3] = platillos;
+
+console.log(platillo1, platillo2, platillo3);
