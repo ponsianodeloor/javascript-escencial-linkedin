80a6e87dcf90051cecc9f5f9b3d76bd5390a2d53
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-18 12:11:12 -0500

    13.5 Busquedas en arreglos
    Filtrar Arreglos

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 "use strict";
 
-//Busqueda en un arreglo
-var platillos = ["Ceviche", "Tacos", "pasta"];
+//Filtrar arreglos
 
 var menu = [
  {nombre: "Ceviche", precio: 20, pais:"Ecuador"},
  {nombre: "Tacos", precio: 30, pais:"Mexico"},
- {nombre: "pasta", precio: 40, pais:"Italia"}
+ {nombre: "pasta", precio: 40, pais:"Italia"},
+ {nombre: "frejoles", precio: 15, pais:"Mexico"}
 ];
 
-var numPlatillo = platillos.findIndex(platillo => platillo =='Tacos');
-console.log("Platillo Numero: " + numPlatillo);
-
-numPlatillo = menu.findIndex(platillo => platillo.nombre =='pasta');
-console.log("Platillo Numero: " + numPlatillo);
+var resultado = null;
+resultado = menu.find(platillo => platillo.pais == "Mexico");
+console.log(resultado);
+resultado = menu.filter(platillo => platillo.pais == "Mexico");
+console.log(resultado);
