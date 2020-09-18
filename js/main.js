2d213e84ffc3b6a2f440186710496def70d3f0d2
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-18 14:01:17 -0500

    14.1.- Conociendo el DOM y el BOM
    Propiedades y metodos del dom

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
 "use strict";
 
 var boton = document.getElementById('boton');
-boton.addEventListener('click', function(){
- console.log("hola que hace");
 
- window.location.href = "https://apptics.com.ec";
+var todosLosParrafos = document.getElementsByTagName('p');
+var parrafosPorClase = document.getElementsByClassName('principal')[0].textContent;
+
+var foto = document.createElement('img');
+foto.src = "img/foto1.jpg";
+foto.name = "Foto1";
+foto.width = 400;
+document.body.appendChild(foto);
+
+foto.addEventListener('click', function(){
+ if (foto.name === "Foto1") {
+  foto.src = "img/foto2.jpg";
+  foto.name = "Foto2";
+ }else {
+  foto.src = "img/foto1.jpg";
+  foto.name = "Foto1";
+ } //dentro de la condicion podemos indicar con la propiedad this ejm: this.name ==="Foto1"
+});
+
+boton.addEventListener('click', function(){
+  console.log(todosLosParrafos);
+  console.log(parrafosPorClase);
+  this.style.backgroundColor = "Black";
+  this.style.color = "White";
 });
