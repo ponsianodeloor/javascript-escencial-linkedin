4824ec6b039ae06ee5023fe1c74cf51280d7927a
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-18 17:39:40 -0500

    14.4.- Conociendo el DOM y el BOM
    Trabajar con promesas

--- js/main.js
+++ js/main.js
@@ -4,16 +4,44 @@
 
 var boton = document.getElementById('boton');
 var contenedor = document.getElementById('container');
+var contenedorBanderas = document.getElementById('banderas');
 var post = null;
 
 boton.addEventListener('click', function(){
- fetch("https://jsonplaceholder.typicode.com/posts").then(data => data.json()).then(data =>{
+ getPost().then(data => data.json()).then(data =>{
   post = data;
   mostrarDatos(post);
+  return getCountries();
+ }).then(data => data.json()).then(countries =>{
+  mostrarBanderas(countries);
  });
 });
 
+function getPost(){
+ return fetch("https://jsonplaceholder.typicode.com/posts");
+}
+
+function getCountries(){
+ return fetch("https://restcountries.eu/rest/v2/all");
+}
+
+function mostrarBanderas(countries){
+ contenedorBanderas.innerHTML = "";
+
+ countries.map((country, i)=>{
+  let bandera = document.createElement('img');
+
+  bandera.src = country.flag;
+  bandera.width = '20';
+  bandera.height = '20';
+  bandera.title = country.name;
+
+  contenedorBanderas.appendChild(bandera);
+ });
+}
+
 function mostrarDatos(){
+ contenedor.innerHTML = "";
  post.map((post, i)=>{
   let titulo = document.createElement('h1');
   let contenido = document.createElement('p');
