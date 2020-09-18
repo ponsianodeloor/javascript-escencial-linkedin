2f6f8501491fc41c55541edc14f563674ba89dc2
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-18 13:02:53 -0500

    14.- Conociendo el DOM y el BOM
    Entender el dom y el bom

--- js/main.js
+++ js/main.js
@@ -1,17 +1,8 @@
 "use strict";
 
-//Validación de elementos de un arreglo
-var resultado = null;
-var menu = [
- {nombre: "Ceviche", precio: 20, pais:"Ecuador"},
- {nombre: "Tacos", precio: 30, pais:"Mexico"},
- {nombre: "pasta", precio: 40, pais:"Italia"},
- {nombre: "frejoles", precio: 15, pais:"Mexico"}
-];
+var boton = document.getElementById('boton');
+boton.addEventListener('click', function(){
+ console.log("hola que hace");
 
-resultado = menu.some(platillo => platillo.precio < 20);
-console.log('Hay platillos de 20 Dolares', resultado);
-
-
-resultado = menu.every(platillo => platillo.precio < 50);
-console.log('Hay platillos que cuesten menos de 50 Dolares', resultado);
+ window.location.href = "https://apptics.com.ec";
+});
