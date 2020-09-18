76b4dab1055f8012dfdedf9af219bf26661e67de
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-18 15:51:25 -0500

    14.2.- Conociendo el DOM y el BOM
    Propiedades y métodos del BOM

--- js/main.js
+++ js/main.js
@@ -3,27 +3,14 @@
 var boton = document.getElementById('boton');
 
 var todosLosParrafos = document.getElementsByTagName('p');
-var parrafosPorClase = document.getElementsByClassName('principal')[0].textContent;
+var parrafosPorClase = document.getElementsByClassName('principal')[0];
+console.log('Height', window.innerHeight);
+console.log('Height', window.innerWidth);
 
-var foto = document.createElement('img');
-foto.src = "img/foto1.jpg";
-foto.name = "Foto1";
-foto.width = 400;
-document.body.appendChild(foto);
-
-foto.addEventListener('click', function(){
- if (foto.name === "Foto1") {
-  foto.src = "img/foto2.jpg";
-  foto.name = "Foto2";
- }else {
-  foto.src = "img/foto1.jpg";
-  foto.name = "Foto1";
- } //dentro de la condicion podemos indicar con la propiedad this ejm: this.name ==="Foto1"
-});
+localStorage.setItem('email', 'el email es ponsianodeloor@gmail.com');
 
 boton.addEventListener('click', function(){
-  console.log(todosLosParrafos);
-  console.log(parrafosPorClase);
-  this.style.backgroundColor = "Black";
-  this.style.color = "White";
+ var email = localStorage.getItem('email');
+ console.log('El email de localStorage es :', email);
+ parrafosPorClase.innerHTML = email;
 });
