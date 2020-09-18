b96a67a9c5c342e00cc0198e47faf0401775268d
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-18 18:56:18 -0500

    16.- Programacion orientada a objetos
    Trabajando con clases en la programación orientada a objetos

--- js/main.js
+++ js/main.js
@@ -1,59 +1,12 @@
 "use strict";
 
-//obtener datos con fetch
+//trabajando con clases
 
-var boton = document.getElementById('boton');
-var contenedor = document.getElementById('container');
-var contenedorBanderas = document.getElementById('banderas');
-var post = null;
+class Pantalla{
+ constructor(){
 
-boton.addEventListener('click', function(){
- getPost().then(data => data.json()).then(data =>{
-  post = data;
-  mostrarDatos(post);
-  return getCountries();
- }).then(data => data.json()).then(countries =>{
-  mostrarBanderas(countries);
- }).catch(error => {
-     mensaje.classList.toggle('hide');
-     mensaje.innerHTML = error;
-     setTimeout(()=>mensaje.classList.toggle('hide'), 5000);
- });
-});
-
-function getPost(){
- return fetch("https://jsonplaceholder.typicode.comm/posts");
+ }
 }
 
-function getCountries(){
- return fetch("https://restcountries.eu/rest/v2/all");
-}
-
-function mostrarBanderas(countries){
- contenedorBanderas.innerHTML = "";
-
- countries.map((country, i)=>{
-  let bandera = document.createElement('img');
-
-  bandera.src = country.flag;
-  bandera.width = '20';
-  bandera.height = '20';
-  bandera.title = country.name;
-
-  contenedorBanderas.appendChild(bandera);
- });
-}
-
-function mostrarDatos(){
- contenedor.innerHTML = "";
- post.map((post, i)=>{
-  let titulo = document.createElement('h1');
-  let contenido = document.createElement('p');
-
-  titulo.innerHTML = (i +1) + " - " + post.title;
-  contenido.innerHTML = post.body;
-
-  contenedor.appendChild(titulo);
-  contenedor.appendChild(contenido);
- });
-}
+const tvSala = new Pantalla();
+const tvHabitacion = new Pantalla();
