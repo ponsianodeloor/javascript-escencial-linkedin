773c6e7d9b560bb34b43fd2857c17f260ee21150
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-17 20:44:36 -0500

    Iterando con arreglos for

--- js/main.js
+++ js/main.js
@@ -1,19 +1,7 @@
 "use strict";
 
-//Desestructuracion de arreglos
+//Iterando con arreglos for
 var platillos = ["Ceviche", "Tacos", "pasta"];
-/*
-var platillo1 = platillos[0];
-var platillo2 = platillos[1];
-var platillo3 = platillos[2];
-*/
-
-/*
-var platillo1 = null;
-var platillo2 = null;
-var platillo3 = null;
-*/
-
-var [platillo1, platillo2, platillo3] = platillos;
-
-console.log(platillo1, platillo2, platillo3);
+for (let platillo in platillos) {
+ console.log(platillos[platillo]);
+}
