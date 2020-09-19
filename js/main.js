ba21f446d56ec07b6f6db9eb76fdb810174e2c5c
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-18 20:41:52 -0500

    16.1.- Programacion orientada a objetos
    Sus métodos y propiedades

--- js/main.js
+++ js/main.js
@@ -3,10 +3,28 @@
 //trabajando con clases
 
 class Pantalla{
- constructor(){
+ constructor(marca, modelo, pulgadas){
+  this.marca = marca;
+  this.modelo = modelo;
+  this.pulgadas = pulgadas;
+ }
 
+ set peso(value){
+  this.kgs = value.trim();
+ }
+ get peso(){
+  return this.kgs;
+ }
+ encender(){
+  console.log(`La pantalla: marca ${this.marca} se ha encendido`);
+ }
+ volumen(){
+  console.log(`El volumen se ha modificado`);
+ }
+ info(){
+  console.log(`La informacion de la tv es la siguiente: marca ${this.marca} modelo ${this.modelo} pulgadas ${this.pulgadas}`);
  }
 }
 
-const tvSala = new Pantalla();
-const tvHabitacion = new Pantalla();
+const tvSala = new Pantalla('Samsung', 'Curve', '52');
+const tvHabitacion = new Pantalla('LG', 'LSD', '30');
